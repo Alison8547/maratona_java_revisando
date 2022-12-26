5873b148162fc53445430b42d3f7f77d081eaccf
Alison8547 <alisonailson100@hotmail.com>
2022-12-26 11:13:31 -0300

    heranca parte 1

---
+++ src/main/java/br/com/maratonajava/poo/heranca/dominio/Pessoa.java
@@ -0,0 +1,35 @@
+package br.com.maratonajava.poo.heranca.dominio;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private Endereco endereco;
+
+    public void imprimir() {
+        System.out.printf("Nome: %s%nCpf: %s%nEndereco -> Rua: %s | Cep: %s%n", this.nome, this.cpf, this.endereco.getRua(), this.endereco.getCep());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ src/main/java/br/com/maratonajava/poo/heranca/test/HerancaTest1.java
@@ -0,0 +1,33 @@
+package br.com.maratonajava.poo.heranca.test;
+
+import br.com.maratonajava.poo.heranca.dominio.Endereco;
+import br.com.maratonajava.poo.heranca.dominio.Funcionario;
+import br.com.maratonajava.poo.heranca.dominio.Pessoa;
+
+public class HerancaTest1 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setCep("84944-224");
+        endereco.setRua("Rua da liberdade");
+
+        Endereco endereco2 = new Endereco();
+        endereco2.setCep("14445-224");
+        endereco2.setRua("Rua da prata");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setCpf("111-111-111-11");
+        pessoa.setNome("Alison");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprimir();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Tiago");
+        funcionario.setCpf("222-223-444-55");
+        funcionario.setSalario(1500.0d);
+        funcionario.setEndereco(endereco2);
+
+        System.out.println("--------");
+        funcionario.imprimir();
+    }
+}
