9e5a042c4f8c2f31bce9350ecaf37c3b4019b11e
Alison8547 <alisonailson100@hotmail.com>
2022-12-27 16:45:32 -0300

    sobrescrita toString

---
+++ src/main/java/br/com/maratonajava/poo/sobrescrita/test/AnimeTest1.java
@@ -0,0 +1,10 @@
+package br.com.maratonajava.poo.sobrescrita.test;
+
+import br.com.maratonajava.poo.sobrescrita.dominio.Anime;
+
+public class AnimeTest1 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Hunter x Hunter");
+        System.out.println(anime);
+    }
+}
