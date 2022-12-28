4d85fc5ce0c6d90f7a6590d3eb96281190a06635
Alison8547 <alisonailson100@hotmail.com>
2022-12-28 00:35:24 -0300

    feat: modificador final pt2 tipo referencia

--- src/main/java/br/com/maratonajava/poo/modificadorfinal/dominio/Carro.java
+++ src/main/java/br/com/maratonajava/poo/modificadorfinal/dominio/Carro.java
@@ -1,8 +1,11 @@
 package br.com.maratonajava.poo.modificadorfinal.dominio;
 
+import br.com.maratonajava.poo.modificadorfinal.Comprador;
+
 public class Carro {
     private String nome;
     public static final double VELOCIDADE_MAXIMA = 250;
+    public final Comprador COMPRADOR = new Comprador();
 
     public String getNome() {
         return nome;
--- src/main/java/br/com/maratonajava/poo/modificadorfinal/test/CarroTest1.java
+++ src/main/java/br/com/maratonajava/poo/modificadorfinal/test/CarroTest1.java
@@ -6,5 +6,9 @@ public class CarroTest1 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_MAXIMA);
+
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Jiraya");
+        System.out.println(carro.COMPRADOR);
     }
 }
