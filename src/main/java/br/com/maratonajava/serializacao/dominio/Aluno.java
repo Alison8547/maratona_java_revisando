8480713232230c670f5c780ec477e433a24119c3
Alison8547 <alisonailson100@hotmail.com>
2023-01-18 14:46:03 -0300

    feat: Serialization

---
+++ src/main/java/br/com/maratonajava/serializacao/dominio/Aluno.java
@@ -0,0 +1,88 @@
+package br.com.maratonajava.serializacao.dominio;
+
+import br.com.maratonajava.serializacao.Turma;
+
+import java.io.*;
+
+public class Aluno implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7443963119478417513L;
+
+    private Long id;
+    private String nome;
+    private transient String password;
+    private static final String NOME_ESCOLA = "Dev Dojo";
+    private transient Turma turma;
+
+    public Aluno(Long id, String nome, String password) {
+        System.out.println("Dentro do construtor do Aluno");
+        this.id = id;
+        this.nome = nome;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", password='" + password + '\'' +
+                ", NOME_ESCOLA='" + NOME_ESCOLA + '\'' +
+                ", Turma='" + turma + '\'' +
+                '}';
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream oss) {
+        try {
+            oss.defaultWriteObject();
+            oss.writeUTF(turma.getNome());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois) {
+        try {
+            ois.defaultReadObject();
+            String nomeTurma = ois.readUTF();
+            turma = new Turma(nomeTurma);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
